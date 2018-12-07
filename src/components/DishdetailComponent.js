acecf469fcf799ced0796c801b25fd29a194dc11
Claudette Morell <claudette.morell@gmail.com>
2018-12-07 14:43:48 -0500

    completed the in-class exercises and created/imported new component for homework

---
+++ src/components/DishdetailComponent.js
@@ -0,0 +1,19 @@
+import { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+
+
+class Dishdetail extends Component  {
+	constructor(props) {
+		super(props)
+	};
+
+	render(){
+		return "got the dish details"
+
+	};
+
+
+}
+
+
+export default Dishdetail;
