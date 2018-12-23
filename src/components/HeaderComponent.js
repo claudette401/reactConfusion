551d0005e1f43db3cc909d456d013c65d2c6dcea
Claudette Morell <claudette.morell@gmail.com>
2018-12-22 20:15:03 -0500

    added fetch error handling for dishes, comments and promos

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -83,7 +83,7 @@ class Header extends Component {
 		        <Jumbotron>
 		        	<div className="container">
 		        	<div className="row row-header">
-		        	<div className="col-12 col-sm-6">
+		        	<div className="col-12 col-sm-9">
 		        		<h1>Ristorante con Fusion</h1>
 		        		<p>We take inspiration from the Worlds best cuisines, and create a unique fusion experience. Our lipsmacking creations will tickle your culinary senses!</p>
 		        	</div>
